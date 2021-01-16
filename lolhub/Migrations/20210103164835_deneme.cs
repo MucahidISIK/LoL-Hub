8c4c6bb836dc457def34c461518d5b1bacfcef86
Mücahid IŞIK <56644702+MucahidISIK@users.noreply.github.com>
2021-01-16 21:57:33 +0300

    Add files via upload

---
+++ lolhub/Migrations/20210103164835_deneme.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace lolhub.Migrations
+{
+    public partial class deneme : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Paylasimlar",
+                columns: table => new
+                {
+                    PaylasimID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Baslik = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Icerik = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VLink = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GLink = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    KullaniciAd = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paylasimlar", x => x.PaylasimID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Paylasimlar");
+        }
+    }
+}

2811006b1745145a2f87b98034b7ed2591a4817f
Mücahid IŞIK <56644702+MucahidISIK@users.noreply.github.com>
2021-01-16 21:56:52 +0300

    Add files via upload

---
+++ lolhub/Data/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using lolhub.Models;
+using lolhub.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace lolhub.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Paylasim> Paylasimlar { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-NQFHQFF; Database=DBlolhub; trusted_connection=true;");
+        }
+
+    }
+}
